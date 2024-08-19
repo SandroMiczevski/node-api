fa645a52165dc44bc1403d636672dd79851bfd21
Sandro Miczevski Jr <sandro.mck@outlook.com>
2024-08-18 23:12:30 -0700

    Added tests for LoginRoute to ensure apropriate statsCode response when the httpRequest is sent null or body cannot be found.

--- src/presentation/routes/route.test.js
+++ src/presentation/routes/route.test.js
@@ -1,16 +1,19 @@
 class LoginRouter{
     route(httpRequest){
-        const {email , password} = httpRequest.body
-
+        
         const httpResponse = {
             statusCode : 200
         }
+        
+        if (!httpRequest || !httpRequest.body) return (httpResponse.statusCode = 500) 
+
+        const {email , password} = httpRequest.body
 
         if (email == null || password == null){
             httpResponse.statusCode = 400;
             return httpResponse;
         }
-        
+
         return httpResponse
     }
 }
@@ -39,5 +42,18 @@ describe('Login Router', () => {
         }
         const httpResponse = sut.route(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
+    })
+
+    test('Returns statusCode 500 when LoginRoute is receiveing empty httpRequest', () =>{
+        const sut = new LoginRouter();
+        const httpResponse = sut.route();
+        expect(httpResponse).toBe(500);
+    })
+
+    test ('Return statusCode 500 if httpRequest has no body', () => {
+        const sut = new LoginRouter()
+        const httpRequest = {}
+        const httpResponse = sut.route(httpRequest);
+        expect(httpResponse).toBe(500)
     })
 });
