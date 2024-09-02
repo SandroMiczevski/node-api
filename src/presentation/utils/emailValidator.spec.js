8bf773454f5ac333a1630e55cff7e0ae449932c8
Sandro Miczevski Jr <sandro.mck@outlook.com>
2024-09-01 23:13:44 -0700

    Added a test to ensure that emailValidator is receiving the right parameters

--- src/presentation/utils/emailValidator.spec.js
+++ src/presentation/utils/emailValidator.spec.js
@@ -2,21 +2,37 @@ const validator = require('validator')
 
 class EmailValidator {
   isValid (email) {
+    this.email = email
     return validator.isEmail(email)
+  }
+}
+
+const makeSut = () => {
+  const sut = new EmailValidator()
+
+  return {
+    sut
   }
 }
 
 describe('Email Validator', () => {
   test('Returns true if valid email is provided', () => {
-    const sut = new EmailValidator()
+    const { sut } = makeSut()
     const emailIsValid = sut.isValid('valid_email@email.com')
     expect(emailIsValid).toBe(true)
   })
 
   test('Returns false if email provided is false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const { sut } = makeSut()
     const emailIsValid = sut.isValid('invalid_email@email.com')
     expect(emailIsValid).toBe(false)
+  })
+
+  test('emailValidator is receiving the right parameter', () => {
+    const email = 'email@email.com'
+    const { sut } = makeSut()
+    sut.isValid(email)
+    expect(validator.email).toBe(email)
   })
 })
